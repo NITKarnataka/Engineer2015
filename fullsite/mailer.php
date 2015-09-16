90c437e6c8446ff06203ff0bda0612fcddd51e0a
connektivion <connektivion@gmail.com>
2015-09-17 02:19:48 +0530

    re mailer fix

--- fullsite/mailer.php
+++ fullsite/mailer.php
@@ -1,7 +1,5 @@
 <?php
 
-if(!$_POST) exit;
-
 $response = array( 'success' => false );
 
 $formData = file_get_contents( 'php://input' );
@@ -14,7 +12,7 @@ if($data->name!=''&&$data->number!=''&&$data->college!=''&&$data->email!=''){
                               // Set email format to HTML
 	$subject = 'Engineer Campus Ambassedor Registration';
 	$body = 'Hey dude,<br>'.$data->name.' from '.$data->college.' has registered<br> '.'Email: '.$data->email.'<br>Number: '.$data->number.'<br>year: '.$data->year.'<br>stream: '.$data->stream.'<br>location: '.$data->location.'<br>Has held any position: '.$data->held.'<br>Description if yes: '.$data->desc.'<br>Facebook :'.$data->flink.'<br>Twitter :'.$data->tlink;
-	$address = "rajatmittal18@gmail.com"
+	$address = "rajatmittal18@gmail.com";
 	if(mail($address, $subject, $body)) {
 		 $response[ 'success' ] = true;
 	} else {
