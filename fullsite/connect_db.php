18e693e9a4770390b1a5c089e143db3b4971ed8a
connektivion <connektivion@gmail.com>
2015-09-17 22:58:51 +0530

    ca mailer wrapper

--- fullsite/connect_db.php
+++ fullsite/connect_db.php
@@ -1,7 +1,7 @@
 <?php
 if(!(mysql_connect('localhost','enginee8','engi2015@NITK'))||!(mysql_select_db('engiee8_engi')))
 {
-	echo"we are temporarily down..cant connect";
-die();
+	echo "we are temporarily down..cant connect";
+	die();
 }
 ?>
--- fullsite/mailer.php
+++ fullsite/mailer.php
@@ -20,22 +20,23 @@ if($data->name!=''&&$data->number!=''&&$data->college!=''&&$data->email!=''){
 	$query = "INSERT INTO `engineer2015`(`name`, `mobile`, `email`, `college`, `location`, `branch`,`year`,`rfor`) VALUES ('$data->name','$number','$data->email','$data->college','$data->location','$data->stream','$year','$data->rfor')";
 	$query_run = mysql_query($query);
 	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-                              // Set email format to HTML
-	$subject = "Engineer Campus Ambassedor Registration";
-    $headers = "MIME-Version: 1.0\r\nContent-Type: text/plain; charset=UTF-8";
-	$body = "Hey dude,\r\n".$data->name." from ".$data->college." has registered\r\n "."Email: ".$data->email."\r\nNumber: ".$data->number."\r\nyear: ".$data->year."\r\nstream: ".$data->stream."\r\nlocation: ".$data->location."\r\nHas held any position: ".$data->held."\r\nDescription if yes: ".$data->desc."\r\nFacebook :".$data->flink."\r\nTwitter :".$data->tlink;
-	$address = "rajatmittal18@gmail.com";
-	if(mail($address, $subject, $body,$headers)&&$query_run) {
-		$response[ 'success' ] = true;
-		$contacter_name = $data->name;
-		$contacter_mail = $data->email;
-		$contacter_subject = "Campus Ambassedor Registration";
-		$contacter_body = "Dear ".$contacter_name.",\r\nThank you for registering..\r\nWe will contact you again..\r\n..";
-		mail($contacter_mail, $contacter_subject , $contacter_body,$headers);
-	} else {
+    if($query_run){                          // Set email format to HTML
+		$subject = "Engineer Campus Ambassedor Registration";
+	    $headers = "MIME-Version: 1.0\r\nContent-Type: text/plain; charset=UTF-8";
+		$body = "Hey dude,\r\n".$data->name." from ".$data->college." has registered\r\n "."Email: ".$data->email."\r\nNumber: ".$data->number."\r\nyear: ".$data->year."\r\nstream: ".$data->stream."\r\nlocation: ".$data->location."\r\nHas held any position: ".$data->held."\r\nDescription if yes: ".$data->desc."\r\nFacebook :".$data->flink."\r\nTwitter :".$data->tlink;
+		$address = "rajatmittal18@gmail.com";
+		if(mail($address, $subject, $body,$headers)) {
+			$response[ 'success' ] = true;
+			$contacter_name = $data->name;
+			$contacter_mail = $data->email;
+			$contacter_subject = "Campus Ambassedor Registration";
+			$contacter_body = "Dear ".$contacter_name.",\r\nThank you for registering..\r\nWe will contact you again..\r\n..";
+			mail($contacter_mail, $contacter_subject , $contacter_body,$headers);
+		} else {
 
-	   $response[ 'success' ] = false;
+		   $response[ 'success' ] = false;
 
+		}
 	}
 	echo json_encode( $response );
 }
