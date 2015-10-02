f8b9e8fa0cbe631f60149d8ec6b4a0c192e6c878
connektivion <connektivion@gmail.com>
2015-10-03 03:32:26 +0530

    debug check agaimn

--- fullsite/check.php
+++ fullsite/check.php
@@ -1,22 +1,19 @@
 <?php
 echo 'sasa';
 	require 'connect_db.php';
-	if(isset($_POST['uname'])&&isset($_POST['pwd'])){
-		$uname = $_POST['uname'];
-		$pwd = $_POST['pwd'];
-		echo $uname.' '.$pwd;
-		if($uname!=''&&$pwd!=''){
-			$query='SELECT * from `usertable` WHERE `uname` = "$uname" AND `pwd` = "$pwd"';
-			$query_run=mysql_query($query);
-			if(mysql_num_rows(mysql_fetch_assoc($query_run))==1){
-				$_SESSION['logged'] = true;
-				header('Location: home.php');
-			}else{
-				$_SESSION['logged'] = false;
-				$_SESSION['errmsg'] = 'wrong user biactch';
-				header('Location: clogin.php');
-				
-			}
+	if(isset($_POST["uname"])&&isset($_POST["pwd"])&&!empty($_POST["uname"])&&!empty($_POST["pwd"])){
+		$uname = $_POST["uname"];
+		$pwd = $_POST["pwd"];
+		$query = 'SELECT * from `usertable` WHERE `uname` = "$uname" AND `pwd` = "$pwd"';
+		$query_run = mysql_query($query);
+		if(mysql_num_rows($query_run)==1){
+			$_SESSION["logged"] = true;
+			header('Location: home.php');
+		}else{
+			$_SESSION["logged"] = false;
+			$_SESSION["errmsg"] = 'wrong user biactch';
+			echo 'whwewwewewewewe';
+			
 		}
 	}else{
 		echo 'wtf';
