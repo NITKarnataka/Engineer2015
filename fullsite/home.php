d9b6ac9b2631f692dc339513f418d7a11a167b29
connektivion <connektivion@gmail.com>
2015-10-03 23:33:16 +0530

    home changes

--- fullsite/home.php
+++ fullsite/home.php
@@ -1,6 +1,6 @@
 <?php
-	//if(isset($_SESSION['logged'])&&$_SESSION['logged']){
-		require 'connect_db.php';
+	require 'connect_db.php';
+	if(isset($_SESSION['logged'])&&$_SESSION['logged']){
 		$query = "SELECT * from `engineer2015` WHERE 1 ORDER BY `rfor` ASC";
 		$query_run = mysql_query($query);
 		$pre='dummy';
@@ -14,14 +14,27 @@
 			$pre = $var['rfor'];
 		}	
 		for($i=0;$i<sizeof($arr);$i++)
-			echo $arr[$i]." <br/>";
+			echo $arr[$i];
 ?>
-	<link rel="stylesheet" type="text/css" href="plugins/bootstrap/bootstrap.min.css">
-	<table class="table table-striped">
-	</table>
+	<script type="text/javascript" src="plugins/angular/angular.min.js"></script>
+	<script type="text/javascript" src="plugins/angular/ng-router.js"></script>
+	<body ng-app="Register" ng-controller="RegisterController">
+		<link rel="stylesheet" type="text/css" href="plugins/bootstrap/bootstrap.min.css">
+		<table class="table table-striped">
+		</table>
+	</body>
+	<script>
+		Register = angular.module('Register');
+		Register.controller('RegisterController', function($scope, $location,$http) {
+			$scope.events = <?php echo json_encode($arr) ?>;
+			$scope.details = <?php echo json_encode(mysql_fetch_array($query_run)); ?>
+			console.log($scope.events);
+			console.log($scope.details);
+		});
+	</script>
 
 <?php
-	// }else{
-	// 	header("Location: clogin.php")
-	// }
+	}else{
+		header("Location: clogin.php")
+	}
 ?>
