a15eb1090baa537d2038e493adba4ba2aeeb03a3
connektivion <connektivion@gmail.com>
2015-10-04 00:13:11 +0530

    home6

--- fullsite/home.php
+++ fullsite/home.php
@@ -5,6 +5,7 @@
 		$query_run = mysql_query($query);
 		$pre='dummy';
 		$arr=array();
+		$count = array();
 		$final = array();
 		while($var = mysql_fetch_assoc($query_run)){
 			if($var['rfor']!=''){
@@ -14,25 +15,43 @@
 				array_push($final,$var);
 			}
 			$pre = $var['rfor'];
-		}	
-		for($i=0;$i<sizeof($arr);$i++)
-			echo $arr[$i];
+		}
+		for($i=0;$i<sizeof($arr);$i++){
+			$query2 = 'SELECT `id` from `engineer2015` WHERE `rfor`="'.$arr[$i].'"';
+			$query_run2 = mysql_query($query2);
+			array_push($count,mysql_num_rows($query2));
+		}
 ?>
 	<html ng-app="Register">
 		<script type="text/javascript" src="plugins/angular/angular.min.js"></script>
 		<script type="text/javascript" src="plugins/angular/ng-router.js"></script>
 		<body ng-controller="RegisterController">
 			<link rel="stylesheet" type="text/css" href="plugins/bootstrap/bootstrap.min.css">
-			<table class="table table-striped">
-			</table>
+				<table class="table table-striped">
+					<tr>
+						<th>slno.</th>
+						<th>committee</th>
+						<th>count</th>
+					</tr>
+					<tr ng-repeat='event in events'>
+						<td>{{$index+1}}</td>
+						<td>{{event}}</td>
+						<td>{{count[$index]}}</td>
+					</tr>
+				</table>
+				<div ng-show="">
+					<table class="table table-striped">
+					</table>
+				</div>
 		</body>
 		<script type="text/javascript">
 			 Register = angular.module('Register', []);
 			 Register.controller('RegisterController', function($scope, $location,$http) {
-				var events = <?php echo json_encode($arr); ?>;
-				var details = <?php echo json_encode($final); ?>;
-				console.log(events);
-				console.log(details);
+				$scope.events = <?php echo json_encode($arr); ?>;
+				$scope.details = <?php echo json_encode($final); ?>;
+				$scope.count = <?php echo json_encode($count); ?>;
+				console.log($scope.events);
+				console.log($scope.details);
 			 });
 		</script>
 	</html>
