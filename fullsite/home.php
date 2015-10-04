2e1db73c90d65b5556ffbc006d4476717bb0a750
connektivion <connektivion@gmail.com>
2015-10-04 08:23:49 +0530

    download as csv

--- fullsite/home.php
+++ fullsite/home.php
@@ -79,6 +79,7 @@
 					<div>
 						<span ng-repeat="item in mailList">{{participants[item].email+','}}</span>
 					</div>
+					<button class="btn btn-success" onclick='downloadCSV({ filename: {{selected +".csv"}} });'>Download As CSV</button>
 					<table class="table table-striped">
 						<tr>
 							<th>Sl No</th>
@@ -166,6 +167,58 @@
 					else
 						addItem($scope.mailList,item);
 				}
+
+			    function convertArrayOfObjectsToCSV(args) {
+			        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+			        data = args.data || null;
+			        if (data == null || !data.length) {
+			            return null;
+			        }
+
+			        columnDelimiter = args.columnDelimiter || ',';
+			        lineDelimiter = args.lineDelimiter || '\n';
+
+			        keys = Object.keys(data[0]);
+
+			        result = '';
+			        result += keys.join(columnDelimiter);
+			        result += lineDelimiter;
+
+			        data.forEach(function(item) {
+			            ctr = 0;
+			            keys.forEach(function(key) {
+			                if (ctr > 0) result += columnDelimiter;
+
+			                result += item[key];
+			                ctr++;
+			            });
+			            result += lineDelimiter;
+			        });
+
+			        return result;
+    			}
+
+			    function downloadCSV(args) {
+			        var data, filename, link;
+
+			        var csv = convertArrayOfObjectsToCSV({
+			            data: $scope.participants
+			        });
+			        if (csv == null) return;
+
+			        filename = args.filename || 'export.csv';
+
+			        if (!csv.match(/^data:text\/csv/i)) {
+			            csv = 'data:text/csv;charset=utf-8,' + csv;
+			        }
+			        data = encodeURI(csv);
+
+			        link = document.createElement('a');
+			        link.setAttribute('href', data);
+			        link.setAttribute('download', filename);
+			        link.click();
+			    }
 
 			 });
 		</script>
