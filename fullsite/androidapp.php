0033f1fc1bfca3c1fb327fed361ff5e288658a48
connektivion <connektivion@gmail.com>
2015-10-06 15:17:00 +0530

    add to db the app downloads

--- fullsite/androidapp.php
+++ fullsite/androidapp.php
@@ -1,5 +1,7 @@
 <?php
-$file_name = 'Engi\'15.apk'; //$_GET['f'] has the link to the file like http://mydomain.com/file/android.apk
-
-header('location: '.$file_name);
+	$file_name = 'Engi\'15.apk'; //$_GET['f'] has the link to the file like http://mydomain.com/file/android.apk 
+	require 'connect_db.php';
+	$query = 'UPDATE apptable SET downloads = downloads + 1 WHERE id = 1';
+	mysql_query($query);
+	header('location: '.$file_name);
 ?>
