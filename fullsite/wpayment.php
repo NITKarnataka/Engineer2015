b9bba7575ba71adbfcaa9a022d8b4d504b4ddecb
connektivion <connektivion@gmail.com>
2015-10-07 05:53:47 +0530

    fix7

--- fullsite/wpayment.php
+++ fullsite/wpayment.php
@@ -1,6 +1,6 @@
 <?php
 
-	require './connect_db.php';
+	require 'connect_db.php';
 	$response[ 'success' ] = false;
 	$formInput= file_get_contents( 'php://input' );
 	$formData = json_decode( $formInput );
@@ -8,10 +8,10 @@
 	$query = "SELECT * FROM `usertable` WHERE `uname`='$formData->uname' AND `pwd`='$formData->pwd'";
 	$query_run = mysql_query($query);
 	if(mysql_num_rows($query_run)==1){
-		$query2 = "UPDATE `engineer2015` SET `send` = '$formData->send' WHERE id='$formData->id'";
+		$query2 = "UPDATE `engineer2015` SET `send` = '$formData->send' WHERE `id`='$formData->id'";
 		if(mysql_query($query2))
 			$response[ 'success' ] = true;
-		else $response[ 'success' ] = false
+		else $response[ 'success' ] = false;
 
 	}
 	echo json_encode( $response );
