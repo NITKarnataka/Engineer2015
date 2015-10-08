49b3b03f4d538e2336f699b55d33e6d6c991f964
connektivion <connektivion@gmail.com>
2015-10-08 07:42:31 +0530

    fixed

--- fullsite/getupdates.php
+++ fullsite/getupdates.php
@@ -4,13 +4,24 @@
 	$formInput= file_get_contents( 'php://input' );
 	$formData = json_decode( $formInput );
 
-
-	$query2 = "SELECT * FROM `Sheet1` WHERE `".$formData->type."`='1'";
-	$query_run2 = mysql_query($query2);
-	$res2 = array();
-	while($var1 = mysql_fetch_assoc($query_run2)){
-		array_push($res2, $var1);
+	if($formData->type="upcoming"){
+		$query2 = "SELECT * FROM `Sheet1` WHERE `".$formData->type."`='1'";
+		$query_run2 = mysql_query($query2);
+		$res1 = array();
+		while($var1 = mysql_fetch_assoc($query_run2)){
+			array_push($res1, $var1);
+		}
 	}
-	echo json_encode( $res2 );
+
+	if($formData->type="current"){
+		$query3 = "SELECT * FROM `Sheet1` WHERE `current`='1'";
+		$query_run3 = mysql_query($query3);
+		$res1 = array();
+		while($var2 = mysql_fetch_assoc($query_run3)){
+			array_push($res1, $var2);
+		}
+	}
+
+	echo json_encode( $res1 );
 
 ?>
